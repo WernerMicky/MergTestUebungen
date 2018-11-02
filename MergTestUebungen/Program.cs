330ba5e7ade7a363ed78ca6f6d92320273c0b823
Werner <Werner@WERNERM>
2018-11-02 11:20:43 +0100

    create a Project for Unite

--- MergTestUebungen/Program.cs
+++ MergTestUebungen/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MergTestUebungen.DatenBank;
 
 namespace MergTestUebungen
 {
@@ -10,6 +11,35 @@ namespace MergTestUebungen
     {
         static void Main(string[] args)
         {
+            List<customers> alle = LiefereMirAlleKunden();
+            foreach (var item in alle)
+            {
+                Console.WriteLine($"custid: {item.custid } compname: {item.compname} contact: {item.contact} city: {item.city } country: {item.country}");
+            }
+
+            Console.ReadKey();
+
+
         }
+        static List<customers> LiefereMirAlleKunden()
+        {
+            List<customers> all = null;
+            using (SalesEntities e = new SalesEntities())
+            {
+                all = new List<customers>();
+                all = e.customers.ToList();
+            }
+            return all;
+        }
+    }
+    class Customers
+    {
+        public long custid { get; set; }
+        public string compname { get; set; }
+        public string contact { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string currid { get; set; }
+        public short discount { get; set; }
     }
 }
